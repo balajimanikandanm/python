3fa6ba4c5775d3fa7640126cd3d1794c36ea9666
Balaji Manikandan.M <mkbalaji1998@gmail.com>
2019-07-16 14:08:18 +0530

    Create Minabs.py

---
+++ Minabs.py
@@ -0,0 +1,26 @@
+def minAbsSumPair(arr,arr_size): 
+	inv_count = 0
+
+	# Array should have at least 
+	# two elements 
+	if arr_size < 2: 
+		print("Invalid Input") 
+		return
+
+	# Initialization of values 
+	min_l = 0
+	min_r = 1
+	min_sum = arr[0] + arr[1] 
+	for l in range (0, arr_size - 1): 
+		for r in range (l + 1, arr_size): 
+			sum = arr[l] + arr[r]				 
+			if abs(min_sum) > abs(sum):		 
+				min_sum = sum
+				min_l = l 
+				min_r = r 
+
+	print( arr[min_l],  arr[min_r]) 
+  sb=int(input())
+arr = list(map(int,input().split()))
+
+minAbsSumPair(arr, len(arr)); 
