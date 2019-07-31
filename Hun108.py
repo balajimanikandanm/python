d92611d9495f9fcd7756bfbee0f642c6212a14bb
Balaji Manikandan.M <mkbalaji1998@gmail.com>
2019-07-31 15:46:51 +0530

    Create Hun108.py

---
+++ Hun108.py
@@ -0,0 +1,16 @@
+A=int(input())
+bs=str(A)
+kl=[]
+s=[]
+summ=0
+for i in range(0,len(b)):
+    s.append(bs[i])
+kl=list(map(int,s))
+if len(kl)==1:
+    print(kl[0]**2)
+else:
+    for i in range(0,len(kl)-1):
+        summ=summ+(kl[i]**kl[i+1])
+    else:
+        summ=summ+(kl[-1]**kl[0])
+    print(summ)
